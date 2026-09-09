dbd04f5d828abc5050d43f336fe3e932e97af817
Valeria Motorenko <Valeria.Motorenko@tptlive.ee>
2026-09-09 11:35:31 +0300

    Add array examples

--- index.php
+++ index.php
@@ -1,18 +1,38 @@
 <?php
-$test = "Hello";
-$test = "Hello" . "World" . "!";
-$test = $test . "!!!!!";
-$test .= "!!!!!";
-$name = "miha";
-$age = 18;
-$test = $name . " is " . $age . " years old";
-$test = "$name is $age years old";
-$test = "hello\n\nworld";
-$test = <<<END
-cool kakashki here
-OMG
-END;
+$number = array(1, 2, 3);
+$number = [1, 2, 3];
+var_dump($number[1]);
+$number[2] = 5;
+var_dump($number);
+$test = [1, 'dodik', true, [1, 2, 3]];
 var_dump($test);
-var_dump($name);
-var_dump($age);
+var_dump($test[3][1]);
+
+$test = [
+    'name' => 'Popopipi',
+    'age' => 67,
+    'city' => 'Tartu',
+    1,
+    2,
+    3,
+    'color' => 'red',
+    'music' => 'rock',
+    5,
+    100 => 6,
+    7,
+];
+var_dump($test);
+var_dump($test['name']);
+
+$combined = [1, 2, 3] + [4, 5, 6, 7];
+var_dump($combined);
+
+$text = implode('-', $test);
+var_dump($text);
+
+array_push($test, 67, 'bibibibibi', 6677);
+var_dump($test);
+$test[] = 'value';
+unset($test[102]);
+var_dump($test);
 ?>
