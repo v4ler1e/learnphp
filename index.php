32c5c94d821223c8634d7b8d9055eb4e5781332c
Valeria Motorenko <Valeria.Motorenko@tptlive.ee>
2026-09-16 11:27:34 +0300

    add pointer and pass reference example

--- index.php
+++ index.php
@@ -88,6 +88,18 @@ class Box {
  $box2->height = 5;
  var_dump($box2, $box2->volume());
 
+$num1 = 1;
+$num2 = &$num1;
+$num1 = 2;
+var_dump($num1, $num2);
+ 
+$box1 = new Box();
+$box1->width = 1;
+$box2 =clone $box1;
+$box2->width = $box1->width;
+$box1->width = 2;
+var_dump($box1, $box2);
+
 ?>
 
 
