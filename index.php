25bc07fda39cc522bf88cb2746c49e5901274bdb
Valeria Motorenko <Valeria.Motorenko@tptlive.ee>
2026-09-16 11:08:10 +0300

    add class and object example

--- index.php
+++ index.php
@@ -66,6 +66,28 @@ function recursive($i) {
 }
 recursive(0);
 
+class Box {
+    public $width;
+    public $length;
+    public $height;
+ 
+    public function volume() {
+        return $this->width * $this->length * $this->height;
+    }
+ }
+ 
+ $box1 = new Box();
+ $box1->width = 5;
+ $box1->length = 10;
+ $box1->height = 15;
+ var_dump($box1, $box1->volume());
+ 
+ $box2 = new Box();
+ $box2->width = 3;
+ $box2->length = 4;
+ $box2->height = 5;
+ var_dump($box2, $box2->volume());
+
 ?>
 
 
